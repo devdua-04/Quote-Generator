b5df551fae63b9421e385e15049baf28801ab62e
devdua-04 <devdua04@gmail.com>
2025-05-23 19:47:17 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,55 @@
+const url = 'https://random-quote-generator2.p.rapidapi.com/randomQuote';
+const options = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': 'a5e1e41edamsh97f6fa08c8dee71p1e3d33jsnecec3d9ff597',
+        'x-rapidapi-host': 'random-quote-generator2.p.rapidapi.com'
+    }
+};
+
+async function fetchQuote() {
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        const json = JSON.parse(result);
+        quoteText[0].innerHTML = json[0].Quote;
+        quoteAuthor[0].innerHTML = "- "+json[0].Author;
+    } catch (error) {
+        console.error(error);
+    }
+}
+fetchQuote();
+// Fetch a random quote from the API and display it on the page
+
+const quoteContainer = document.getElementsByClassName('quote-box'),
+    quoteText = document.getElementsByClassName('quote-text'),
+    quoteAuthor = document.getElementsByClassName('quote-author'),
+    quoteButton = document.getElementById('generate-quote'),
+    copyButton = document.getElementById('copy-quote');
+
+
+quoteButton.addEventListener('click', async () => {
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        const json = JSON.parse(result);
+        quoteText[0].innerHTML = json[0].Quote;
+        quoteAuthor[0].innerHTML = "- "+json[0].Author;
+
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+copyButton.addEventListener('click', () => {
+    const quote = quoteText[0].innerHTML;
+    const author = quoteAuthor[0].innerHTML;
+    const fullQuote = `${quote} ${author}`;
+    navigator.clipboard.writeText(fullQuote).then(() => {
+        alert('Quote copied to clipboard!');
+    }).catch(err => {
+        console.error('Failed to copy: ', err);
+    });
+});
+
+
